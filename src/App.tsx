8240b389e47b719e29fd11e6d32f18ee19485e81
Hakuryo0413 <100341996+Hakuryo0413@users.noreply.github.com>
2024-05-07 22:38:17 +0700

    git commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,227 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Box from "@mui/material/Box";
-import AddIcon from "@mui/icons-material/Add";
-import "./App.css";
-import {
-  AppBar,
-  Button,
-  Container,
-  CssBaseline,
-  IconButton,
-  ListItemSecondaryAction,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import * as React from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Divider from "@mui/material/Divider";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import SendIcon from "@mui/icons-material/Send";
-import ListItemButton from "@mui/material/ListItemButton";
-import EditCalendarIcon from "@mui/icons-material/EditCalendar";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import { Task } from "@mui/icons-material";
-import TaskListItem from "./TaskListItem";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
 
-export interface Task {
+import * as React from "react";
+
+export interface Item {
   id: String;
-  task: String;
-  date: String;
+  name: String;
+  image: String;
+  discountPercent: Number;
+  availableQuanlity: Number;
+  price: Number;
 }
+
+const mockData = [
+  {
+    id: 1,
+    name: 'Laptop ASUS VivoBook 14 A412FA-EK734T (14" FHD/i5-10210U/8GB/512GB SSD/Intel UHD)',
+    image:
+      "https://lh3.googleusercontent.com/OPNIaDml5ZTh9653Gqtxc8BaWFqHZmSjaMs0ou321y3YWNR_68HybgMlVsflBN147gok7WxnsmrB7rKq9Z4a1mhnaS2lSYUPMA=rw-w300",
+    price: 25800000,
+    discountPercent: 10,
+    seller: "Xiaomi",
+    relatedProducts: [
+      {
+        id: 2,
+        name: 'Laptop Lenovo (14" 4GB/128GB SSD/Intel UHD)',
+        image:
+          "https://lh3.googleusercontent.com/TvxcE7fes0NtPyjDUqmvVlbdgj63tujZtJCroBtrrMXu1j-MC1Rns56ia6z2USjinoa5gp_GfbthX5BRThBHEdBXFbWXBQ4K=rw-w300",
+
+        availableQuantity: 3,
+        price: 12000000,
+        discountPercent: 5,
+      },
+      {
+        id: 3,
+        name: "Apple Macbook Pro 16 inch Retina display",
+        image:
+          "https://lh3.googleusercontent.com/TsvY9ecw4JYAwF0Ez-M5h_xc1qtX2bwLS21Zc_CbKy_qgIB0OEkbSnEREiXqxZEgnaPCyhLvSh04q7Z1GQlRFeyh5sprWto=rw-w300",
+        discountPercent: "",
+        availableQuantity: 4,
+        price: 15190000,
+      },
+      {
+        id: 4,
+        name: 'Laptop Dell (16" 8GB/256GB SSD/Intel UHD)',
+        image:
+          "https://lh3.googleusercontent.com/awNX33cUhBXFLT7Wv9aNBfKo9wiwiwLgPi2JfNgZKHGWTmYZ1N5AMQNE4HZT2AuZMZ1zX2Exk7UwQ0d-irm_sjcUwClzZTlW=rw-w300",
+        discountPercent: "",
+        availableQuantity: 3,
+        price: 19000000,
+      },
+    ],
+  },
+];
+
 function App() {
-  const date = new Date();
-  const [task, setTask] = useState<string>("Demo task");
-  const [tasks, setTasks] = useState<Task[]>([]);
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <Box
-          component="section"
-          sx={{
-            p: 2,
-            border: "1px grey",
-            backgroundColor: "#f9f6ff",
-            width: "500px",
-          }}
-        >
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-            <Box>
-              <Typography variant="h6" component="h2">
-                TO DO
+
+      <Container maxWidth="sm" sx={{ justifyContent: "center" }}>
+        {mockData.map((item) => (
+          <Card key={item.id} style={{ marginBottom: 16 }} elevation={0}>
+            <CardMedia
+              component="img"
+              width={"100%"}
+              image={item.image}
+              alt={item.name}
+            />
+            <CardContent>
+              <Typography
+                variant="h6"
+                style={{ color: "red", fontWeight: "bold" }}
+                gutterBottom
+              >
+                {(
+                  (item.price * (100 - item.discountPercent)) /
+                  100
+                ).toLocaleString()}
+                VND
               </Typography>
-            </Box>
-
-            <Button variant="contained" startIcon={<AddIcon />}>
-              New task
-            </Button>
-          </Box>
-          <Box>
-            <TaskListItem date={date} task={task} />
-            {/*  <List
-              sx={{ width: "100%", marginTop: 2, bgcolor: "background.paper" }}
-            >
-              <ListItemButton sx={{ display: "block" }}>
-                <Typography variant="subtitle2">
-                  {date.toDateString()}
+              <Box style={{ display: "flex" }}>
+                <Typography
+                  variant="caption"
+                  sx={{ textDecoration: "line-through", opacity: 0.25 }}
+                >
+                  {item.price.toLocaleString()} VND
                 </Typography>
+                {item.discountPercent && (
+                  <Typography
+                    variant="caption"
+                    style={{ color: "red", marginLeft: 4 }}
+                  >
+                    -{item.discountPercent}%
+                  </Typography>
+                )}
+              </Box>
+              <Typography
+                variant="body1"
+                style={{ fontWeight: "bold" }}
+                gutterBottom
+              >
+                {item.name}
+              </Typography>
+              <Box style={{ display: "flex" }}>
+                <Typography variant="caption" style={{ color: "gray" }}>
+                  By
+                </Typography>
+                <Typography
+                  variant="caption"
+                  style={{
+                    color: "#36a5ff",
+                    marginLeft: 4,
+                    textTransform: "uppercase",
+                  }}
+                >
+                  {item.seller}
+                </Typography>
+              </Box>
+              <Typography
+                variant="body1"
+                style={{ fontWeight: "bold", marginTop: 16, marginBottom: 8 }}
+                gutterBottom
+              >
+                Sản phẩm liên quan
+              </Typography>
+              <div style={{ display: "flex", overflowX: "auto" }}>
+                {item.relatedProducts.map((relatedProduct) => (
+                  <Card
+                    key={relatedProduct.id}
+                    sx={{
+                      minWidth: 200,
+                      marginRight: 2,
+                      border: 1,
+                      borderColor: "#edecf3",
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image={relatedProduct.image}
+                      title={relatedProduct.name}
+                    />
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="body2"
+                        component="div"
+                        style={{
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          whiteSpace: "nowrap",
+                        }}
+                      >
+                        {relatedProduct.name}
+                      </Typography>
+                      <Typography variant="body2" color="#edae61">
+                        Còn {relatedProduct.availableQuantity} sản phẩm
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        sx={{ color: "red", fontWeight: "bold" }}
+                      >
+                        {(
+                          (relatedProduct.price *
+                            (100 - Number(relatedProduct.discountPercent))) /
+                          100
+                        ).toLocaleString()}
+                        VND
+                      </Typography>
+                      {/* Assuming you have a discountPercent property for related products */}
+                      <Box sx={{ display: "flex" }}>
+                        <Typography
+                          variant="caption"
+                          sx={{ textDecoration: "line-through", opacity: 0.25 }}
+                        >
+                          {relatedProduct.price.toLocaleString()}
+                        </Typography>
+                        {relatedProduct.discountPercent && (
+                          <Typography
+                            variant="caption"
+                            sx={{ color: "red", marginLeft: 1 }}
+                          >
+                            -{relatedProduct.discountPercent}%
+                          </Typography>
+                        )}
+                      </Box>
+                    </CardContent>
+                    <CardActions
+                      sx={{
+                        justifyContent: "center",
+                        justifyItems: "center",
+                        marginTop: -2,
+                      }}
+                    >
+                      <Button
+                        size="medium"
+                        sx={{
+                          color: "red",
+                          border: 1,
 
-                <Box sx={{ display: "flex" }}>
-                  <ListItemText
-                    primary={task}
-                    primaryTypographyProps={{
-                      variant: "subtitle1",
-                      fontWeight: "bold",
-                    }}
-                  />
-                  <ListItemIcon
-                    onClick={() => {
-                      alert("edit");
-                    }}
-                  >
-                    <EditCalendarIcon sx={{ color: "blue" }} />
-                  </ListItemIcon>
-                  <ListItemIcon
-                    onClick={() => {
-                      alert("delete");
-                    }}
-                  >
-                    <DeleteOutlineIcon sx={{ color: "red" }} />
-                  </ListItemIcon>
-                </Box>
-              </ListItemButton>
-            </List> */}
-          </Box>
-        </Box>
+                          justifyContent: "center",
+                          justifyItems: "center",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        Thêm vào giỏ
+                      </Button>
+                    </CardActions>
+                  </Card>
+                ))}
+              </div>
+            </CardContent>
+          </Card>
+        ))}
       </Container>
     </React.Fragment>
   );
